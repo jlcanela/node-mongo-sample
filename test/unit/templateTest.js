4ab3ee7dcad2bb55b180ad7b554fbd64b29c7d86
Jean-Luc CANELA <jlcanelam@gmail.com>
2017-01-22 10:35:11 +0100

    adding template test

---
+++ test/unit/templateTest.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const Promise = require('bluebird').Promise;
+const should = chai.should();
+
+describe('template test', function(){
+    it('should be ok', function(){
+      return Promise.resolve(true).should.be.fulfilled;
+    })
+  })
