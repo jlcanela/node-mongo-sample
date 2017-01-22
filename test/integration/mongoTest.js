d37c7c653df7f23ba36cef1913bc5abf5274f781
Jean-Luc CANELA <jlcanelam@gmail.com>
2017-01-22 13:40:36 +0100

    updating tests with co

--- test/integration/mongoTest.js
+++ test/integration/mongoTest.js
@@ -2,18 +2,21 @@ const chai = require('chai');
 chai.use(require('chai-as-promised'));
 const Promise = require('bluebird').Promise;
 const should = chai.should();
+const co = require('co');
 
-//process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
 const mongoClient = require('../../src/lib/mongo.js');
 
 describe('mongo test', function(){
   it('should access to collection', function(){
-    mongoClient().collection('test_insert')
-    .then(function(collection){
-      return collection.insert({a:2});
-    }).then(function(docs){
-      return collection.count();
-    }).should.eventually.be.eql(0);
+    return co(function *(){
+      const millis = (new Date()).getTime();
+      const coll = yield mongoClient().collection(`test_insert_${millis}`);
+      const r = yield coll.insert({a:2});
+      const cn = yield coll.count();
+      yield coll.drop();
+      return cn;
+    }).should.eventually.be.eql(1);
   });
 })
